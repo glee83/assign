08b4d41adf79a2fd30c8e86502fa69e331ac1bc8
Glee Albert <gleealbert307@gmail.com>
2021-10-11 20:22:12 +0100

    second commit

--- home.php
+++ home.php
@@ -8,19 +8,26 @@
     <link rel="stylesheet" href="./bootstrap-5.0.2-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./styles/style.css">
 </head>
-<body>
-    <form class="w-50 d-flex justify-content-center align-item-center" action="signup.php" method="POST" >
+<body >
+    <form class="p-2" action="signup.php" method="POST" >
+        <h3 class="p-1 mb-3 text-center" > Hello, please Sign-up</h3>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Name:</label>
             <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password:</label>
             <input type="password" name="password" class="form-control" id="exampleInputPassword1">
         </div>
+
+        <div class="mb-5" >
+            <span class="float-end">Already have an account? 
+                <a href="#" class="text-primary">login</a>
+            </span>
+        </div>
         
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary mx-auto ml-5">Submit</button>
     </form>
 
     <script src="./bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
